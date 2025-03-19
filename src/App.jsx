f0587f49a2cf87c3fc43899174e1716d00642f3e
brilliahib <brilliahib21@gmail.com>
2025-03-19 19:40:08 +0700

    feat: create new activity

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
 import CreateActivityPage from "./pages/CreateActivityPage";
+import UpdateActivityPage from "./pages/UpdateActivityPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login" element={<UpdateActivityPage />} />
         <Route path="/create" element={<CreateActivityPage />} />
       </Routes>
     </Router>
--- src/pages/UpdateActivityPage.jsx
+++ src/pages/UpdateActivityPage.jsx
@@ -1,4 +1,4 @@
-export default function LoginPage() {
+export default function UpdateActivityPage() {
   return (
     <div className="flex justify-center items-center h-screen">
       <div>Login Page</div>
